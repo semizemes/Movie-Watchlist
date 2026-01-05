6bf3a8891fffad41942e2afbe0f687f0aed6153b
Abbos Soatmurodov <soatmurodovabbos@gmail.com>
2026-01-05 20:59:11 +0500

    feat: enhance movie search functionality and update layout styles

--- index.js
+++ index.js
@@ -1,10 +1,56 @@
-const movie = document.getElementById("movie")
+const movieForm = document.getElementById("movie-form");
+let movieIdArr = [];
+let moviesHtmlArr = [];
 
+movieForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
 
-fetch("https://omdbapi.com/?s=batman&apikey=9835bb97")
-.then(res => res.json())
-.then(data => console.log(data))
+  let movie = Object.fromEntries(new FormData(e.target)).movie;
 
-document.getElementById("btn").addEventListener("click", (e) => {
-    console.log(movie.value);
-})
+  const res = await fetch(
+    `https://omdbapi.com/?s=${movie}&plot=short&apikey=9835bb97`
+  );
+  const data = await res.json();
+  movieIdArr = data.Search.map((i) => i.imdbID);
+
+  renderSearchedMovies();
+
+  document.getElementById("movie").value = "";
+});
+
+async function renderSearchedMovies() {
+  moviesHtmlArr = await movieIdArr.map(async (movieId) => {
+    const res = await fetch(
+      `https://www.omdbapi.com/?i=${movieId}&apikey=9835bb97`
+    );
+    const data = await res.json();
+    console.log(data);
+    return `
+            <div class="searched-movie">
+                <img src="${data.Poster}" alt='"${data.Title}" poster image' />
+
+                <div class="movie-data">
+                <div class="info-block">
+                    <div class="movie-header">
+                    <h2>${data.Title}</h2>
+                    <i class="fa-solid fa-star"></i>
+                    <p>${data.imdbRating}</p>
+                    </div>
+                </div>
+                <div class="movie-main">
+                    <p>${data.Runtime}</p>
+                    <p>${data.Genre}</p>
+                    <div class="watchlist">
+                    <i class="fa-solid fa-circle-plus"></i>
+                    <p>Watchlist</p>
+                    </div>
+                </div>
+                <div class="plot">${data.Plot}</div>
+                </div>
+            </div>
+        `;
+  });
+  const arr = await Promise.all(moviesHtmlArr);
+
+  document.getElementById("content").innerHTML = arr.join(" ");
+}
